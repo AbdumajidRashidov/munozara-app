5153c4664f4b9b3cc65cc92508aa28d475391eb7
ABdumajid Rashidov <abdumajidrashidov44@gmail.com>
2021-05-07 23:05:51 +0500

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose')
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 5000;
 const {MONGOURL} = require('./config/keys')
 
 
